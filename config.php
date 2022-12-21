f50faa24df279c74110c5820f866b3f438a7e4d4
Mehrdad <100048050+Mehrdadnka@users.noreply.github.com>
2022-12-22 02:27:59 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	class configDb
+	{
+		protected $servername,$username,$password;
+
+		function configdb()
+		{
+			$this->servername = "localhost";
+			$this->username = "root";
+			$this->password = "";
+		}
+	}
+	
+?>
---
+++ conn.php
@@ -0,0 +1,34 @@
+<?php
+    include('config.php');
+
+    class Connection extends configDb
+    {
+        public $conn;
+        protected $servername,$username,$password;
+        
+        function __construct() 
+        {
+            $this->conn = new mysqli($this->servername,$this->username,$this->password);
+        
+            //check connection
+            if($this->conn->connect_error)
+            {
+                die("Connection failed: ".$this->conn->connect_error);
+    
+            }
+            else
+            {
+                echo "Connected successfully";
+            }
+            return $this->conn;
+        }
+
+        function __destruct()
+        {
+            $this->servername = NULL;
+            $this->username = NULL;
+            $this->password = NULL;
+            $this->conn =NULL;
+        }
+    }
+    ?>
